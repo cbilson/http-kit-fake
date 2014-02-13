ab9c198bfdb6e8361c4ac680de3bd289ec507b7f
Chris Bilson <cbilson@pobox.com>
2014-02-13 04:32:09 -0800

    change http-kit dependency to 2.1.16

--- project.clj
+++ project.clj
@@ -4,5 +4,5 @@
   :license {:name "MIT License"
             :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [http-kit "2.1.12"]
+                 [http-kit "2.1.16"]
                  [robert/hooke "1.3.0"]])
